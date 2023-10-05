c0e9b3ec40a7a66dc7ed4e8353339505b42d6dca
Michele Riva <ciao@micheleriva.it>
2023-10-05 12:23:07 +0200

    first commit

---
+++ src/index.ts
@@ -0,0 +1,52 @@
+interface Highlight {
+  positions: Array<{ start: number, end: number }>
+  toString: () => string
+}
+
+export interface HighlightOptions {
+  caseSensitive?: boolean
+  wholeWords?: boolean
+  HTMLTag?: string
+  CSSClass?: string
+}
+
+const defaultOptions: HighlightOptions = {
+  caseSensitive: false,
+  wholeWords: false,
+  HTMLTag: 'mark',
+  CSSClass: 'orama-highlight'
+}
+
+export function highlight (text: string, searchTerm: string, options: HighlightOptions = defaultOptions): Highlight {
+  const { caseSensitive, wholeWords, HTMLTag, CSSClass } = options
+  const regexFlags = caseSensitive ? 'g' : 'gi'
+  const boundary = wholeWords ? '\\b' : ''
+  const searchTerms = (caseSensitive ? searchTerm : searchTerm.toLowerCase()).split(/\s+/).join('|')
+  const regex = new RegExp(`${boundary}${searchTerms}${boundary}`, regexFlags)
+  const positions: Array<{ start: number, end: number }> = []
+  const highlightedParts: string[] = []
+
+  let match
+
+  const sourceText = caseSensitive ? text : text.toLowerCase()
+  let lastEnd = 0
+
+  while ((match = regex.exec(sourceText)) !== null) {
+    const start = match.index
+    const end = start + match[0].length - 1
+
+    positions.push({ start, end })
+
+    highlightedParts.push(text.slice(lastEnd, start))
+    highlightedParts.push(`<${HTMLTag} class="${CSSClass}">${match[0]}</${HTMLTag}>`)
+
+    lastEnd = end + 1
+  }
+
+  highlightedParts.push(text.slice(lastEnd))
+
+  return {
+    positions,
+    toString: () => highlightedParts.join('')
+  }
+}
