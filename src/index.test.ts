d3b03bd4f90ec0c77ae321c81677c20e0a422a9b
Michele Riva <ciao@micheleriva.it>
2023-10-05 12:40:38 +0200

    fixes lint

--- src/index.test.ts
+++ src/index.test.ts
@@ -3,12 +3,11 @@ import assert from 'node:assert'
 import { highlight } from './index.js'
 
 describe('default configuration', () => {
-
-  it('should correctly highlight a text' , () => {
+  it('should correctly highlight a text', () => {
     const text1 = 'The quick brown fox jumps over the lazy dog'
     const searchTerm1 = 'fox'
     const expectedResult1 = 'The quick brown <mark class="orama-highlight">fox</mark> jumps over the lazy dog'
-  
+
     const text2 = 'Yesterday all my troubles seemed so far away, now it looks as though they\'re here to stay oh, I believe in yesterday'
     const searchTerm2 = 'yesterday I was in trouble'
     const expectedResult2 = '<mark class="orama-highlight">yesterday</mark> all my <mark class="orama-highlight">trouble</mark>s seemed so far away, now <mark class="orama-highlight">i</mark>t looks as though they\'re here to stay oh, <mark class="orama-highlight">i</mark> bel<mark class="orama-highlight">i</mark>eve <mark class="orama-highlight">i</mark>n <mark class="orama-highlight">yesterday</mark>'
@@ -16,7 +15,6 @@ describe('default configuration', () => {
     assert.strictEqual(highlight(text1, searchTerm1).toString(), expectedResult1)
     assert.strictEqual(highlight(text2, searchTerm2).toString(), expectedResult2)
   })
-
 })
 
 describe('custom configuration', () => {
